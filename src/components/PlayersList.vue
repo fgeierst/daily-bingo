24ee661a628ce31f052fdecaacc4d77cf9760a2d
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-03-04 00:09:55 +0100

    Animal scale animation on enter

--- src/components/PlayersList.vue
+++ src/components/PlayersList.vue
@@ -64,6 +64,21 @@ function unsubscribePlayerEvents() {
 			<li v-for="player in players" 
 				:key="player.id" 
 				:class="{ isBingo: player.isBingo }"
+				v-motion
+					:initial="{
+						scale: 0,
+						opacity: 0,
+					}"
+					:enter="{
+						scale: 1,
+						opacity: 1,
+						transition: {
+							type: 'spring',
+							stiffness: 300,
+							damping: 5,
+							mass: .8,
+						},
+					}"
 			>
 				<div class="avatar">
 					<component :is="animals[player.animal]" class="animal"/>
